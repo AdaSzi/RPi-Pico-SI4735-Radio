da6209477e4c18e7d3d7af4f0fd84bf5e28f8333
AdaSzi115026 <81822538+AdaSzi@users.noreply.github.com>
2023-09-15 23:08:43 +0200

    rds mostly done (static text)

--- src/main.cpp
+++ src/main.cpp
@@ -338,7 +338,8 @@ void showBandName() {
 #include <RotaryEncoder.h>
 
 TFT_eSPI tft = TFT_eSPI();
-TFT_eSprite spr = TFT_eSprite(&tft);
+TFT_eSprite mainDataPanel = TFT_eSprite(&tft);
+TFT_eSprite secondaryDataPanel = TFT_eSprite(&tft);
 
 #define FM_BAND_TYPE 0
 #define MW_BAND_TYPE 1
@@ -346,190 +347,78 @@ TFT_eSprite spr = TFT_eSprite(&tft);
 #define LW_BAND_TYPE 3
 
 
-RotaryEncoder encoder(ENCODER_PIN_A, ENCODER_PIN_B, RotaryEncoder::LatchMode::TWO03);
+RotaryEncoder encoder(ENCODER_PIN_A, ENCODER_PIN_B, RotaryEncoder::LatchMode::FOUR3);
 
 #define color1 0xC638
 #define color2 0xC638
 
-volatile int encoderCount = 0;
+mainScreenData mainData;
+FMScreenData fmData;
 
-int value = 980;
+
 int minimal = 880;
 int maximal = 1080;
 int strength = 0;
 
-float freq = 0.00;
+//float freq = 0.00;
 SI4735 radio;
 
-bool muted = 0;
-int deb = 0;
 
 void setup()
 {  
   Serial.begin(115200);
   Serial.println("\nHello");
 
-  tft.begin();
-  //tft.writecommand(0x11);
-  tft.setRotation(0);
-  tft.fillScreen(TFT_BLACK);
+  setupRadio();
 
+  setupScreen();
+  drawMainDataPanel();
+}
+
+void setupRadio()
+{
   Wire.begin();
   radio.setI2CFastModeCustom(100000);
+  radio.setAudioMuteMcuPin(AUDIO_MUTE);
   radio.getDeviceI2CAddress(RESET_PIN); // Looks for the I2C bus address and set it.  Returns 0 if error
   radio.setup(RESET_PIN, MW_BAND_TYPE);
   delay(200);
   radio.setTuneFrequencyAntennaCapacitor(0);
-  radio.setFM(6400, 10800, 10390, 10);
-  delay(200);
-  freq = radio.getFrequency() / 100.0;
 
-  radio.setVolume(58);
+  radio.setVolume(63);
+  mainData.volume = 63;
 
-  spr.createSprite(320, 170);
-  tft.setTextDatum(4);
+  setFMMode();
+}
+
+void setupScreen()
+{
+  tft.begin();
+  //tft.writecommand(0x11);
+  tft.setRotation(0);
+  tft.fillScreen(TFT_BLACK);
+  
+  mainDataPanel.createSprite(320, 100);
+  secondaryDataPanel.createSprite(320, 100);
+  tft.setTextDatum(TL_DATUM);
   tft.setSwapBytes(true);
   tft.setFreeFont(&Orbitron_Light_24);
-  tft.setTextColor(color1, TFT_BLACK);
-
-  drawSprite();
-}
-
-void readEncoder()
-{
-
-  static int pos = 0;
-
-  encoderCount = 0;
-
-  encoder.tick();
-
-  if (digitalRead(0) == 0)
-  {
-    if (deb == 0)
-    {
-      deb = 1;
-      muted = !muted;
-      // radio.setAudioMute(muted);
-      drawSprite();
-      //delay(200);
-    }
-  }
-  else
-    deb = 0;
-
-  int newPos = encoder.getPosition();
-  if (pos != newPos)
-  {
-
-    if (newPos > pos)
-    {
-      value = value - 1;
-      encoderCount = -1;
-    }
-    if (newPos < pos)
-    {
-      value = value + 1;
-      encoderCount = 1;
-    }
-
-    pos = newPos;
-
-    drawSprite();
-    showStatusSerial();
-  }
-}
-
-void drawSprite()
-{
-
-  // if(muted==false)
-
-  if (encoderCount == 1)
-    radio.frequencyUp();
-  else if (encoderCount == -1)
-    radio.frequencyDown();
-  
-  freq = radio.getFrequency() / 100.0;
-  value = freq * 10;
-
-  // strength=getStrength();
-
-  //spr.fillSprite(TFT_BLACK);
   tft.setTextColor(TFT_WHITE, TFT_BLACK);
-
-  tft.drawFloat(freq, 1, 100, 40, 7);
-  tft.setTextColor(TFT_WHITE, TFT_BLACK);
-
   
-  for (int i = 0; i < strength; i++)
-  {
-    if (i < 9)
-      spr.fillRect(100 + (i * 4), 50 - (i * 1), 2, 4 + (i * 1), TFT_GREEN);
-    else
-      spr.fillRect(100 + (i * 4), 50 - (i * 1), 2, 4 + (i * 1), TFT_RED);
-  }
-  
-  if (radio.getCurrentPilot())
-    tft.drawString("Stereo", 275, 31, 2);
-  else
-    tft.drawString("Mono", 275, 31, 2);
-
-  //spr.drawLine(160, 114, 160, 170, TFT_RED);
-  //spr.pushSprite(0, 170);
+  tft.drawString("ABCI1 ", 1, 200, 1);
+  tft.drawString("ABCI2 ", 1, 250, 2);
+  tft.drawString("ABCI4 ", 1, 300, 4);
+  tft.drawString("ABCI6 ", 1, 350, 6);
+  tft.drawString("ABCI8 ", 1, 400, 8);
 }
 
-int getStrength()
-{
-
-  uint8_t rssi;
-
-  rssi = radio.getCurrentRSSI();
-
-  if ((rssi >= 0) and (rssi <= 1))
-    return 1; // S0
-  if ((rssi > 1) and (rssi <= 1))
-    return 2; // S1
-  if ((rssi > 2) and (rssi <= 3))
-    return  3; // S2
-  if ((rssi > 3) and (rssi <= 4))
-    return  4; // S3
-  if ((rssi > 4) and (rssi <= 10))
-    return  5; // S4
-  if ((rssi > 10) and (rssi <= 16))
-    return 6; // S5
-  if ((rssi > 16) and (rssi <= 22))
-    return 7; // S6
-  if ((rssi > 22) and (rssi <= 28))
-    return  8; // S7
-  if ((rssi > 28) and (rssi <= 34))
-    return 9; // S8
-  if ((rssi > 34) and (rssi <= 44))
-    return 10; // S9
-  if ((rssi > 44) and (rssi <= 54))
-    return 11; // S9 +10
-  if ((rssi > 54) and (rssi <= 64))
-    return 12; // S9 +20
-  if ((rssi > 64) and (rssi <= 74))
-    return 13; // S9 +30
-  if ((rssi > 74) and (rssi <= 84))
-    return 14; // S9 +40
-  if ((rssi > 84) and (rssi <= 94))
-    return 15; // S9 +50
-  if (rssi > 94)
-    return 16; // S9 +60
-  if (rssi > 95)
-    return 17; //>S9 +60
-
-  return 17;
-
-}
 
 void loop()
 {
-  readEncoder();
-  radio.getCurrentReceivedSignalQuality();
-  strength = getStrength();
+  handleEncoder();
+  handleRadio();
+  handleScreen();
+  
 
   if (Serial.available() > 0)
   {
@@ -579,7 +468,322 @@ void loop()
         break;
     }
   }
-  delay(5);
+  //delay(5);
+}
+
+unsigned long nextRadioDataUpdate = 0;
+void handleRadio()
+{
+  if (millis() > nextRadioDataUpdate) {
+    nextRadioDataUpdate = millis() + 500;
+
+    radio.getCurrentReceivedSignalQuality();
+    mainData.freq = radio.getFrequency();
+    mainData.isStereo = radio.getCurrentPilot();
+    mainData.multipath.value = radio.getCurrentMultipath();
+
+    mainData.rssi.value = radio.getCurrentRSSI();
+    mainData.rssi.isLow = radio.getCurrentRssiDetectLow();
+    mainData.rssi.isHigh = radio.getCurrentRssiDetectHigh();
+
+    mainData.snr.value = radio.getCurrentSNR();
+    mainData.snr.isLow = radio.getCurrentSnrDetectLow();
+    mainData.snr.isHigh = radio.getCurrentSnrDetectHigh();
+
+    mainData.multipath.value = radio.getCurrentMultipath();
+    mainData.multipath.isLow = radio.getCurrentMultipathDetectLow();
+    mainData.multipath.isHigh = radio.getCurrentMultipathDetectHigh();
+
+    mainData.agc = radio.isAgcEnabled();
+
+    strength = getStrength(mainData.rssi.value);
+  }
+
+  if(mainData.currentRadioMode == 0){
+
+    char *rdsTime;
+    char *stationName;
+    char *programInfo;
+    char *stationInfo;
+
+    
+    fmData.rdsAvailable = radio.getRdsAllData(&fmData.stationNamePtr, &fmData.stationInfoPtr, &fmData.programInfoPtr, &fmData.rdsTimePtr);
+
+
+    /*radio.getRdsAllData(&stationName, &stationInfo, &programInfo, &rdsTime);
+    if (rdsTime != NULL) {
+      //Serial.println(rdsTime);
+      //strncpy(fmData.rdsTime, rdsTime, 32);
+    }
+
+    if (stationName != NULL) {
+      //Serial.println(stationName);
+      //strncpy(fmData.stationName, stationName, 255);
+    }
+    
+    if (programInfo != NULL) {
+      //Serial.println(programInfo);
+      //strncpy(fmData.programInfo, programInfo, 255);
+    }
+    
+    if (stationInfo != NULL) {
+      //Serial.println(stationInfo);
+      //strncpy(fmData.stationInfo, stationInfo, 255);
+    }*/
+
+
+  }
+}
+
+unsigned long nextSecondaryDataPanelUpdate = 0;
+uint8_t lastCrcMain = 0, lastCrcSecondary = 0;
+void handleScreen()
+{
+  uint8_t crcMain = 0;
+  uint8_t *p = (uint8_t *)&mainData;
+  for(int i = 0; i < sizeof(mainData); i++) {
+    crcMain ^= p[i];
+  }
+  
+  if (crcMain != lastCrcMain) {
+    lastCrcMain = crcMain;
+    drawMainDataPanel();
+  }
+
+  /*if(mainData.currentRadioMode == 0) {
+    
+    uint8_t crcSecondary = 0;
+    uint8_t *p = (uint8_t *)&fmData;
+    for(int i = 0; i < sizeof(fmData); i++) {
+      crcSecondary ^= p[i];
+    }
+    
+    if (crcSecondary != lastCrcSecondary) {
+      lastCrcSecondary = crcSecondary;
+      drawSecondaryDataPanelFM();
+    }
+  }*/
+      //drawSecondaryDataPanelFM();
+
+  if (millis() > nextSecondaryDataPanelUpdate) {
+    nextSecondaryDataPanelUpdate = millis() + 500;
+    drawSecondaryDataPanelFM();
+  }
+}
+
+void drawMainDataPanel()
+{
+  mainDataPanel.fillSprite(TFT_BLACK);
+  mainDataPanel.drawRect(0, 0, 320, 100, TFT_WHITE);
+  
+  //mainDataPanel.setTextColor(TFT_WHITE, TFT_BLACK);
+  mainDataPanel.setTextColor(TFT_WHITE, 0x52AA);
+  
+  mainDataPanel.drawString(radioModes[mainData.currentRadioMode], 1, 1, 4);
+  mainDataPanel.drawFloat(mainData.freq/100.0, 1, 50, 1, 7);
+
+  if(mainData.currentRadioMode == 0) mainDataPanel.drawString("MHz", 200, 1, 4);
+  else mainDataPanel.drawString("kHz", 200, 1, 4);
+
+  char buf[32];
+  sprintf(buf, "Vol: %u", mainData.volume);  
+  mainDataPanel.drawString(buf, 270, 20, 2);
+
+  sprintf(buf, "RSSI: %u dBuV", mainData.rssi.value);
+  if(mainData.rssi.isLow) mainDataPanel.setTextColor(TFT_RED, 0x52AA);
+  if(mainData.rssi.isHigh) mainDataPanel.setTextColor(TFT_GREEN, 0x52AA);
+  mainDataPanel.drawString(buf, 1, 50, 2);
+  mainDataPanel.setTextColor(TFT_WHITE, 0x52AA);
+
+  sprintf(buf, "SNR: %u dB", mainData.snr.value);
+  if(mainData.snr.isLow) mainDataPanel.setTextColor(TFT_RED, 0x52AA);
+  if(mainData.snr.isHigh) mainDataPanel.setTextColor(TFT_GREEN, 0x52AA);
+  mainDataPanel.drawString(buf, 100, 50, 2);
+  mainDataPanel.setTextColor(TFT_WHITE, 0x52AA);
+
+  sprintf(buf, "MULT: %u%%", mainData.multipath.value);
+  if(mainData.multipath.isLow) mainDataPanel.setTextColor(TFT_RED, 0x52AA);
+  if(mainData.multipath.isHigh) mainDataPanel.setTextColor(TFT_GREEN, 0x52AA);
+  mainDataPanel.drawString(buf, 200, 50, 2);
+  mainDataPanel.setTextColor(TFT_WHITE, 0x52AA);
+  
+  if(mainData.agc) mainDataPanel.drawString("AGC", 280, 50, 2);
+
+  for (int i = 0; i < strength; i++)
+  {
+    if (i < 9)
+      mainDataPanel.fillRect(3 + (i * 4), 93 - (i * 1), 2, 4 + (i * 1), TFT_GREEN);
+    else
+      mainDataPanel.fillRect(3 + (i * 4), 93 - (i * 1), 2, 4 + (i * 1), TFT_RED);
+  }
+  
+  /*if (mainData.isStereo)
+    mainDataPanel.drawString("Stereo", 278, 1, 2);
+  else
+    mainDataPanel.drawString("Mono", 278, 1, 2);*/
+  mainDataPanel.drawString((radio.getCurrentPilot()) ? "STEREO" : "MONO", 278, 1, 2);
+
+  mainDataPanel.pushSprite(0, 0);
+}
+
+
+void drawSecondaryDataPanelFM()
+{  
+  /*static char rdsTime[32];
+  static char stationName[255];
+  static char programInfo[255];
+  static char stationInfo[255];*/
+
+  secondaryDataPanel.fillSprite(TFT_BLACK);
+  secondaryDataPanel.drawRect(0, 0, 320, 100, TFT_WHITE);
+
+  //mainDataPanel.setTextColor(TFT_WHITE, TFT_BLACK);
+  mainDataPanel.setTextColor(TFT_WHITE, 0x52AA);
+  tft.setFreeFont(&Orbitron_Light_24);
+  if(fmData.rdsAvailable) secondaryDataPanel.drawString("RDS", 150, 1, 4);
+
+  if (fmData.stationNamePtr != NULL) {
+    strncpy(fmData.stationName, fmData.stationNamePtr, 9);
+  }
+  
+  if (fmData.programInfoPtr != NULL) {
+    strncpy(fmData.programInfo, fmData.programInfoPtr, 65);
+    //secondaryDataPanel.drawString(fmData.programInfo, 2, 30, 4);
+  }
+
+  
+  secondaryDataPanel.drawString(fmData.stationName, 2, 1, 4);
+  secondaryDataPanel.drawString(fmData.programInfo, 2, 30, 4);
+
+
+
+
+
+
+  
+  secondaryDataPanel.pushSprite(0, 101);
+}
+
+void handleEncoder()
+{  
+  static int encoderCount = 0;
+  encoder.tick();
+
+  //encoder.getMillisBetweenRotations();
+  static int pos = 0;
+  static int deb = 0;
+
+  if (digitalRead(ENCODER_PUSH_BUTTON) == 0)
+  {
+    if (deb == 0)
+    {
+      deb = 1;
+      // radio.setAudioMute(muted);
+      drawMainDataPanel();
+      //delay(200);
+    }
+  }
+  else
+    deb = 0;
+
+  if(encoder.getPosition() != encoderCount){
+    Serial.println(encoder.getPosition());
+    encoderCount = encoder.getPosition();
+
+    if(encoderCount != mainData.freq){
+      mainData.freq = encoderCount;
+      if(mainData.currentRadioMode == 0) mainData.freq = encoderCount * 10;
+      else mainData.freq = encoderCount;
+      radio.setFrequency(mainData.freq);
+    }
+  }
+  /*if(encoder.getDirection() == RotaryEncoder::Direction::CLOCKWISE) {
+    radio.frequencyUp();
+    Serial.println("ENC UP");
+  }
+  else if(encoder.getDirection() == RotaryEncoder::Direction::COUNTERCLOCKWISE){
+    radio.frequencyDown();
+    Serial.println("ENC DN");
+  }
+  else if(encoder.getDirection() == RotaryEncoder::Direction::NOROTATION);*/
+  /*int newPos = encoder.getPosition();
+  if (pos != newPos)
+  {
+
+    if (newPos > pos)
+    {
+      value = value - 1;
+      encoderCount = -1;
+    }
+    if (newPos < pos)
+    {
+      value = value + 1;
+      encoderCount = 1;
+    }
+
+    pos = newPos;
+
+    drawMainDataPanel();
+    showStatusSerial();
+  }
+
+  
+  if (encoderCount == 1)
+    radio.frequencyUp();
+  else if (encoderCount == -1)
+    radio.frequencyDown();*/
+}
+
+int getStrength(uint8_t rssi)
+{
+
+  if ((rssi >= 0) and (rssi <= 1))
+    return 1; // S0
+  if ((rssi > 1) and (rssi <= 1))
+    return 2; // S1
+  if ((rssi > 2) and (rssi <= 3))
+    return  3; // S2
+  if ((rssi > 3) and (rssi <= 4))
+    return  4; // S3
+  if ((rssi > 4) and (rssi <= 10))
+    return  5; // S4
+  if ((rssi > 10) and (rssi <= 16))
+    return 6; // S5
+  if ((rssi > 16) and (rssi <= 22))
+    return 7; // S6
+  if ((rssi > 22) and (rssi <= 28))
+    return  8; // S7
+  if ((rssi > 28) and (rssi <= 34))
+    return 9; // S8
+  if ((rssi > 34) and (rssi <= 44))
+    return 10; // S9
+  if ((rssi > 44) and (rssi <= 54))
+    return 11; // S9 +10
+  if ((rssi > 54) and (rssi <= 64))
+    return 12; // S9 +20
+  if ((rssi > 64) and (rssi <= 74))
+    return 13; // S9 +30
+  if ((rssi > 74) and (rssi <= 84))
+    return 14; // S9 +40
+  if ((rssi > 84) and (rssi <= 94))
+    return 15; // S9 +50
+  if (rssi > 94)
+    return 16; // S9 +60
+  if (rssi > 95)
+    return 17; //>S9 +60
+
+  return 17;
+
+}
+
+void setFMMode(){  
+  radio.setFM(6400, 10800, 9660, 10);
+  mainData.currentRadioMode = 0;
+  delay(200);
+  mainData.freq = radio.getFrequency();
+  encoder.setPosition(mainData.freq/10);
+  radio.setRdsConfig(3, 3, 3, 3, 3);
+  radio.setFifoCount(1);
 }
 
 // Instructions
--- src/main.h
+++ src/main.h
@@ -19,9 +19,65 @@ void showHelp();
 void showStatus();
 void showBandName();*/
 
+struct rssiData {
+    uint8_t value = 0;
+    bool isHigh = false;
+    bool isLow = true;
+};
+
+struct snrData {
+    uint8_t value = 0;
+    bool isHigh = false;
+    bool isLow = true;
+};
+
+struct multipathData {
+    uint8_t value = 0;
+    bool isHigh = false;
+    bool isLow = true;
+};
+
+struct mainScreenData {
+    uint8_t currentRadioMode = 0;
+    uint16_t freq = 0;
+    bool isStereo = false;
+    rssiData rssi;
+    snrData snr;
+    multipathData multipath;
+    bool agc = false;
+    uint8_t volume = 0;
+};
+
+struct FMScreenData {
+    bool rdsAvailable = false;
+    char *rdsTimePtr;
+    char *stationNamePtr;
+    char *programInfoPtr;
+    char *stationInfoPtr;
+
+    char rdsTime[25];
+    char stationName[9];
+    char programInfo[65];
+    char stationInfo[33];
+};
+
+char radioModes[][4] = {"FM ", "AM ", "USB", "LSB"};
+
 void setup();
 void loop();
-void drawSprite();
+
+
+void setupRadio();
+void handleRadio();
+void setFMMode();
+int getStrength(uint8_t rssi);
+
+void setupScreen();
+void handleScreen();
+void drawMainDataPanel();
+void drawSecondaryDataPanelFM();
+
+void handleEncoder();
 void showHelp();
 void showStatusSerial();
 
